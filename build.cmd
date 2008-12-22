ce9babf97da51bc38e44fae7cabff4743ff201a0
Kirit Sælensminde <k@kirit.com>
2008-12-22 05:53:20 +0000

    Internet protocol programs now compile and work on Windows.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,7 @@
-svn up .
+@svn up .
+@echo off
+
+rmdir /s /q ..\dist
 
 IF EXIST ..\Boost\install GOTO gotboost
     IF EXIST ..\Boost GOTO boostbuild
@@ -13,6 +16,8 @@ IF EXIST ..\OpenSSL GOTO gotopenssl
     svn co svn://svn.felspar.com/external/OpenSSL ..\OpenSSL
 :gotopenssl
 
-..\bjam -j%NUMBER_OF_PROCESSORS% release --toolset=msvc
-..\dist\bin\ftest -b false ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll
+..\bjam toolset=msvc release
 
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll
+)
