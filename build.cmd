472d98997a2441a3b727fa7a8da80b1071ff8da7
Kirit Sælensminde <k@kirit.com>
2008-12-25 10:24:18 +0000

    Updated the build instructions and the ftest MSVC project so that smoke tests can be debugged more easily.

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 @svn up .
 @echo off
 
+rmdir /s /q ..\dist
+
 IF EXIST ..\Boost\install GOTO gotboost
     IF EXIST ..\Boost GOTO boostbuild
         svn co svn://svn.felspar.com/external/Boost ..\Boost
@@ -14,7 +16,7 @@ IF EXIST ..\OpenSSL GOTO gotopenssl
     svn co svn://svn.felspar.com/external/OpenSSL ..\OpenSSL
 :gotopenssl
 
-..\bjam -j%NUMBER_OF_PROCESSORS% release --toolset=msvc
+..\bjam release toolset=msvc
 
 IF NOT ERRORLEVEL 1 (
     ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-schema-test-smoke.dll
