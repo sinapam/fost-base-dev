8c30392d5552844bdff9f711500b270eb899b88a
Kirit Sælensminde <k@kirit.com>
2009-02-05 07:38:01 +0000

    Merged dbschema back in to trunk.

--- build.cmd
+++ build.cmd
@@ -1,16 +1,17 @@
-<<<<<<< .working
 @svn up .
 @echo off
 
-rmdir /s /q ..\dist
+IF EXIST ..\dist (
+    rmdir /s /q ..\dist
+)
 
 IF EXIST ..\Boost\install GOTO gotboost
     IF EXIST ..\Boost GOTO boostbuild
         svn co svn://svn.felspar.com/external/Boost ..\Boost
     :boostbuild
-    cd ..\Boost
+    pushd ..\Boost
     call build.cmd src
-    cd ..\fost
+    popd
 :gotboost
 
 IF EXIST ..\OpenSSL GOTO gotopenssl
@@ -20,25 +21,25 @@ IF EXIST ..\OpenSSL GOTO gotopenssl
 ..\bjam toolset=msvc release
 
 IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll
+    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll  ..\dist\bin\fost-jsondb-test-smoke.dll ..\dist\bin\fost-schema-test-smoke.dll
 )
-=======
-svn up .
 
-IF EXIST ..\Boost\install GOTO gotboost
-    IF EXIST ..\Boost GOTO boostbuild
-        svn co svn://svn.felspar.com/external/Boost ..\Boost
-    :boostbuild
-    cd ..\Boost
-    call build.cmd src
-    cd ..\fost
-:gotboost
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\fost-schema-test-jsondb-file Cpp\fost-schema-test\jsondb-file.json
+)
 
-IF EXIST ..\OpenSSL GOTO gotopenssl
-    svn co svn://svn.felspar.com/external/OpenSSL ..\OpenSSL
-:gotopenssl
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\fost-jsondb-test-file
+)
 
-..\bjam -j%NUMBER_OF_PROCESSORS% release --toolset=msvc
-..\dist\bin\ftest -b false ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll
 
->>>>>>> .merge-right.r33818
+
+
+
+
+
+
+
+
+
+
