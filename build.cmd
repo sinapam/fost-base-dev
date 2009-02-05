45f8cca8a382ad44df2746f83a6ecdd06646a277
Kirit Sælensminde <k@kirit.com>
2009-02-05 06:23:36 +0000

    These changes should not have been committed to stable yet. They need to go through trunk first.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,4 @@
-@svn up .
-@echo off
-
-rmdir /s /q ..\dist
+svn up .
 
 IF EXIST ..\Boost\install GOTO gotboost
     IF EXIST ..\Boost GOTO boostbuild
@@ -16,16 +13,6 @@ IF EXIST ..\OpenSSL GOTO gotopenssl
     svn co svn://svn.felspar.com/external/OpenSSL ..\OpenSSL
 :gotopenssl
 
-..\bjam toolset=msvc release
+..\bjam -j%NUMBER_OF_PROCESSORS% release --toolset=msvc
+..\dist\bin\ftest -b false ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll
 
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll  ..\dist\bin\fost-jsondb-test-smoke.dll ..\dist\bin\fost-schema-test-smoke.dll
-)
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-schema-test-jsondb-file Cpp\fost-schema-test\jsondb-file.json
-)
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-jsondb-test-file
-)
