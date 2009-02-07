d960b641bf8b24a300747f19c6066fd1aef5d15a
Kirit Sælensminde <k@kirit.com>
2009-02-07 16:35:41 +0000

    Build arguments are now passed on to bjam.

--- build.cmd
+++ build.cmd
@@ -18,7 +18,7 @@ IF EXIST ..\OpenSSL GOTO gotopenssl
     svn co svn://svn.felspar.com/external/OpenSSL ..\OpenSSL
 :gotopenssl
 
-..\bjam toolset=msvc release
+..\bjam toolset=msvc %*
 
 IF NOT ERRORLEVEL 1 (
     ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll  ..\dist\bin\fost-jsondb-test-smoke.dll ..\dist\bin\fost-schema-test-smoke.dll
