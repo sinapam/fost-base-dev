35268fc919f283f4045b70e4f2ae4a8597a75eb0
Kirit Sælensminde <k@kirit.com>
2009-05-12 03:45:00 +0000

    Split out the build script to add a separate compile script.

--- build.cmd
+++ build.cmd
@@ -18,28 +18,4 @@ IF EXIST ..\OpenSSL GOTO gotopenssl
     svn co svn://svn.felspar.com/external/OpenSSL ..\OpenSSL
 :gotopenssl
 
-..\bjam toolset=msvc %*
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\ftest ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll ..\dist\bin\fost-inet-test-smoke.dll  ..\dist\bin\fost-jsondb-test-smoke.dll ..\dist\bin\fost-schema-test-smoke.dll
-)
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-schema-test-jsondb-file Cpp\fost-schema-test\jsondb-file.json
-)
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-jsondb-test-file
-)
-
-
-
-
-
-
-
-
-
-
-
-
+call compile.cmd %*
