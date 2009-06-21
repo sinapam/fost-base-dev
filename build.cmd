3ba83e4f0a5b4bc295072d76d4c77cdeedd5317f
Kirit Sælensminde <k@kirit.com>
2009-06-21 07:40:50 +0000

    Merged all changes from trunk to form new stable release.

--- build.cmd
+++ build.cmd
@@ -1,18 +1,25 @@
-svn up .
+@svn up .
+@echo off
+
+IF EXIST ..\dist (
+    rmdir /s /q ..\dist
+)
 
 IF EXIST ..\Boost\install GOTO gotboost
     IF EXIST ..\Boost GOTO boostbuild
         svn co svn://svn.felspar.com/external/Boost ..\Boost
     :boostbuild
-    cd ..\Boost
+    pushd ..\Boost
     call build.cmd src
-    cd ..\fost
+    popd
 :gotboost
 
 IF EXIST ..\OpenSSL GOTO gotopenssl
     svn co svn://svn.felspar.com/external/OpenSSL ..\OpenSSL
 :gotopenssl
 
-..\bjam -j%NUMBER_OF_PROCESSORS% release --toolset=msvc
-..\dist\bin\ftest -b false ..\dist\bin\fost-core-test-smoke.dll ..\dist\bin\fost-crypto-test-smoke.dll
+call compile.cmd %*
 
+IF NOT ERRORLEVEL 1 (
+    ..\dist\bin\fost-core-test-file-io fost-core-test-file-io.txt
+)
