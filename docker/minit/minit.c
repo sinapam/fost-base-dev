38a009ee0792434c7466dee5db7da1a064614f3c
Kirit Sælensminde <k@kirit.com>
2014-09-01 11:32:35 +0700

    Exit minit if the child process exits
    Also try to report some errors.

--- docker/minit/minit.c
+++ docker/minit/minit.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[], char *env[]) {
     sigset_t set;
-    int status;
+    int status, child;
 
     if (getpid() != 1) {
         return 1;
@@ -18,17 +18,25 @@ int main(int argc, char *argv[], char *env[]) {
     sigfillset(&set);
     sigprocmask(SIG_BLOCK, &set, 0);
 
-    if (fork()) {
-        for (;;) {
-            wait(&status);
+    child = fork();
+    if ( child > 0 ) {
+        for ( ;; ) {
+            if ( wait(&status) == child ) {
+                exit(status);
+            }
         }
+    } else {
+        printf("fork failed\n");
+        exit(EXIT_FAILURE);
     }
 
     sigprocmask(SIG_UNBLOCK, &set, 0);
     setsid();
     setpgid(0, 0);
     if ( argc > 1 ) {
-        return execve(argv[1], argv + 1, env);
+        execve(argv[1], argv + 1, env);
+        printf("execve failed\n");
+        exit(EXIT_FAILURE);
     } else {
         return execve("/etc/rc", (char *[]){ "/etc/rc", 0 }, env);
     }
