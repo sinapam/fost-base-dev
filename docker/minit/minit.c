2a244e3909a74af072f03b545bd3797de89b38d6
Kirit Sælensminde <k@kirit.com>
2014-10-17 12:47:43 +0700

    Add a restart version of the minit iamge.

--- docker/minit/minit.c
+++ docker/minit/minit.c
@@ -10,37 +10,46 @@
 #include <stdio.h> // printf
 
 
+int execute(sigset_t *pset, char *prog, char *argv[], char *env[]) {
+    int child = fork();
+    if ( child > 1 ) {
+        return child;
+    } else if ( child < 0 ) {
+        printf("fork failed\n");
+        exit(EXIT_FAILURE);
+    }
+    // We're now in the child so need to exec the requested program
+    sigprocmask(SIG_UNBLOCK, pset, 0);
+    setsid();
+    setpgid(0, 0);
+    execvpe(prog, argv, env);
+    printf("execvpe failed\n");
+    exit(255);
+}
+
+
 int main(int argc, char *argv[], char *env[]) {
     sigset_t set;
     int status, child;
 
     if (getpid() != 1) {
         return 1;
+    } else if ( argc < 2 ) {
+        printf("Must supply a program to execute");
+        exit(EXIT_FAILURE);
     }
 
     sigfillset(&set);
     sigprocmask(SIG_BLOCK, &set, 0);
 
-    child = fork();
-    if ( child > 0 ) {
-        for ( ;; ) {
-            if ( wait(&status) == child ) {
-                exit(status);
-            }
+    child = execute(&set, argv[1], argv + 1, env);
+    for ( ;; ) {
+        if ( wait(&status) == child ) {
+#ifdef RESTART
+            child = execute(&set, argv[1], argv + 1, env);
+#else
+            exit(status);
+#endif
         }
-    } else if ( child < 0 ) {
-        printf("fork failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    sigprocmask(SIG_UNBLOCK, &set, 0);
-    setsid();
-    setpgid(0, 0);
-    if ( argc > 1 ) {
-        execve(argv[1], argv + 1, env);
-        printf("execve failed\n");
-        exit(255);
-    } else {
-        return execve("/etc/rc", (char *[]){ "/etc/rc", 0 }, env);
     }
 }
